a6a352c4acf1b7d0cb16b0ed713875fe05000985
jlbuild <juliabuildbot@gmail.com>
2026-02-12 21:52:40 +0000

    aws_c_mqtt_jll build 0.13.4+0

--- src/aws_c_mqtt_jll.jl
+++ src/aws_c_mqtt_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("aws_c_mqtt")
-JLLWrappers.@generate_main_file("aws_c_mqtt", UUID("35636aaa-5d3a-56eb-bfb5-a154ce8a9530"))
+JLLWrappers.@generate_main_file("aws_c_mqtt", Base.UUID("35636aaa-5d3a-56eb-bfb5-a154ce8a9530"))
 end  # module aws_c_mqtt_jll
